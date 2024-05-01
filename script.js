f898625b19f1e7597cebd881f971ce54ecfd9bac
Aman Kutlaria <rajaman.rajput09@gmail.com>
2024-05-01 23:27:53 +0530

    update

--- script.js
+++ script.js
@@ -5,94 +5,100 @@
 // BANKAROO APP
 
 // Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
+// const account1 = {
+//   owner: 'Jonas Schmedtmann',
+//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+//   interestRate: 1.2, // %
+//   pin: 1111,
+// };
 
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
+// const account2 = {
+//   owner: 'Jessica Davis',
+//   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+//   interestRate: 1.5,
+//   pin: 2222,
+// };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
+// const account3 = {
+//   owner: 'Steven Thomas Williams',
+//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
+//   interestRate: 0.7,
+//   pin: 3333,
+// };
 
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
+// const account4 = {
+//   owner: 'Sarah Smith',
+//   movements: [430, 1000, 700, 50, 90],
+//   interestRate: 1,
+//   pin: 4444,
+// };
 
-const accounts = [account1, account2, account3, account4];
+// const accounts = [account1, account2, account3, account4];
 
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+// // Elements
+// const labelWelcome = document.querySelector('.welcome');
+// const labelDate = document.querySelector('.date');
+// const labelBalance = document.querySelector('.balance__value');
+// const labelSumIn = document.querySelector('.summary__value--in');
+// const labelSumOut = document.querySelector('.summary__value--out');
+// const labelSumInterest = document.querySelector('.summary__value--interest');
+// const labelTimer = document.querySelector('.timer');
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
+// const containerApp = document.querySelector('.app');
+// const containerMovements = document.querySelector('.movements');
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+// const btnLogin = document.querySelector('.login__btn');
+// const btnTransfer = document.querySelector('.form__btn--transfer');
+// const btnLoan = document.querySelector('.form__btn--loan');
+// const btnClose = document.querySelector('.form__btn--close');
+// const btnSort = document.querySelector('.btn--sort');
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+// const inputLoginUsername = document.querySelector('.login__input--user');
+// const inputLoginPin = document.querySelector('.login__input--pin');
+// const inputTransferTo = document.querySelector('.form__input--to');
+// const inputTransferAmount = document.querySelector('.form__input--amount');
+// const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+// const inputCloseUsername = document.querySelector('.form__input--user');
+// const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
-  containerMovements.innerHTML = '';
+// const displayMovements = function (movements) {
+//   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
+//   movements.forEach(function (mov, i) {
+//     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
-    const html = `
-    <div class="movements__row">
-      <div class="movements__type movements__type--${type}">${
-      i + 1
-    } ${type}</div>
+//     const html = `
+//     <div class="movements__row">
+//       <div class="movements__type movements__type--${type}">${
+//       i + 1
+//     } ${type}</div>
 
-      <div class="movements__value">${mov}</div>
-  </div>`;
-    containerMovements.insertAdjacentHTML('afterbegin', html);
-  });
-};
+//       <div class="movements__value">${mov}</div>
+//   </div>`;
+//     containerMovements.insertAdjacentHTML('afterbegin', html);
+//   });
+// };
 
-displayMovements(account1.movements);
+// displayMovements(account1.movements);
 
-const createUsernames = function (accs) {
-  accs.forEach(function (acc) {
-    acc.username = acc.owner
-      .toLowerCase()
-      .split(' ')
-      .map(word => word[0])
-      .join('');
-  });
-};
+// const calcPrintBalance = function (movements) {
+//   const balance = movements.reduce((acc, mov) => acc + mov, 0);
+//   labelBalance.textContent = `${balance}€`;
+// };
 
-createUsernames(accounts);
-console.log(accounts);
+// calcPrintBalance(account1.movements);
+
+// const createUsernames = function (accs) {
+//   accs.forEach(function (acc) {
+//     acc.username = acc.owner
+//       .toLowerCase()
+//       .split(' ')
+//       .map(word => word[0])
+//       .join('');
+//   });
+// };
+
+// createUsernames(accounts);
 
 //----------------------------------------------------------------------------------
 
@@ -153,35 +159,21 @@ console.log(accounts);
 //     }
 // }
 
-// Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+//
 
-// const checkDogs = function (dogsJulia, dogsKate) {
-//   const dogsJuliaCorrect = dogsJulia.slice(1, 3);
-//   // dogsJuliaCorrect.splice(0, 1);
-//   // dogsJuliaCorrect.splice(-2);
-//   const dogs = dogsJuliaCorrect.concat(dogsKate);
-//   // const dogs = [...dogsJuliaCorrect, ...dogsKate];
-//   dogs.forEach(function (age, i) {
-//     if (age > 3) {
-//       console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
-//     } else {
-//       console.log(
-//         `Dog number ${i + 1} is still pupppy, and is ${age} years old`
-//       );
-//     }
-//   });
-// };
+// //Reduce
 
-// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// const balance = movements.reduce(function (acc, curr, i, arr) {
+//   return acc + curr;
+// });
+// console.log(balance);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const max = movements.reduce(function (acc, mov) {
+//   if (acc > mov) return acc;
+//   else return mov;
+// }, movements[0]);
 
-//Reduce
-
-const balance = movements.reduce(function (acc, curr, i, arr) {
-  return acc + curr;
-});
-console.log(balance);
+// console.log(max);
 
 //Filter method
 
@@ -222,3 +214,54 @@ console.log(balance);
 // });
 
 // console.log(movementDes);
+
+// Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   const dogsJuliaCorrect = dogsJulia.slice(1, 3);
+//   // dogsJuliaCorrect.splice(0, 1);
+//   // dogsJuliaCorrect.splice(-2);
+//   const dogs = dogsJuliaCorrect.concat(dogsKate);
+//   // const dogs = [...dogsJuliaCorrect, ...dogsKate];
+//   dogs.forEach(function (age, i) {
+//     if (age > 3) {
+//       console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
+//     } else {
+//       console.log(
+//         `Dog number ${i + 1} is still pupppy, and is ${age} years old`
+//       );
+//     }
+//   });
+// };
+
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+// const calcAverageHumanAge = function (ages) {
+//   const ageInHumanYears = ages.map(function (age) {
+//     if (age <= 2) {
+//       return 2 * age;
+//     } else return 16 + age * 4;
+//   });
+//   console.log(ageInHumanYears);
+//   const newAge = ageInHumanYears.filter(function (agee) {
+//     if (agee > 18) return agee;
+//   });
+//   console.log(newAge);
+
+//   const avgAge = newAge.reduce(function (acc, age) {
+//     const avg = acc + age / newAge.length;
+//     return avg;
+//   }, 0);
+//   console.log(avgAge);
+// };
+
+// calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const euroToUsd = 1.1;
+const totalDesopitUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * euroToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDesopitUSD);
